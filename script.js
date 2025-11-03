c46ec941319f52bee80e4eafe830d8f1696f3347
omarroddy <omarroddy109@gmail.com>
2025-11-03 09:37:54 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,63 +6,63 @@
     { 
       id: 1, 
       name: "AI Solar Max 400W Panel", 
-      price: 2800, 
+      price: 109.99, 
       category: "panels", 
       img: "fig.6.jpg" 
     },
     { 
       id: 2, 
       name: "SmartPanel Ultra 600W", 
-      price: 4500, 
+      price: 139.99, 
       category: "panels", 
       img: "fig.7.jpg" 
     },
     { 
       id: 3, 
       name: "EcoLite 300W Compact", 
-      price: 1950, 
+      price: 99.99, 
       category: "panels", 
       img: "fig.8.jpg" 
     },
     { 
       id: 4, 
       name: "Solar Lantern 50W", 
-      price: 320, 
+      price: 89.99, 
       category: "lamps", 
       img: "fig.9.jpg" 
     },
     { 
       id: 5, 
       name: "Solar Flood Light 200W", 
-      price: 780, 
+      price: 79.99, 
       category: "lamps", 
       img: "fig10.jpg" 
     },
     { 
       id: 6, 
       name: "Deep Cycle Battery 12V/200Ah", 
-      price: 1500, 
+      price: , 109.99
       category: "batteries", 
       img: "fig.11.jpg" 
     },
     { 
       id: 7, 
       name: "Solar Charge Controller 60A", 
-      price: 620, 
+      price: 89.99, 
       category: "accessories", 
       img: "fig.12.jpg"
     },
     { 
       id: 8, 
       name: "Inverter Pro 2.5kVA", 
-      price: 2500, 
+      price: 69.99, 
       category: "accessories", 
       img: "fig.13.jpg" 
     },
     { 
       id: 9, 
       name: "Complete Solar Home Kit", 
-      price: 8900, 
+      price: 59.99, 
       category: "panels", 
       img: "fig.12.jpg" 
     }
